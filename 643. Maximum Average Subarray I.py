19cbe5f95cbda29421e23b6c176deaf307a7c378
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-04-03 16:52:59 -0400

    Upload 643

--- 643. Maximum Average Subarray I.py
+++ 643. Maximum Average Subarray I.py
@@ -17,3 +17,18 @@ class Solution:
                 Max = max(Max, (total[i]-total[i-k])/k)
 
         return Max
+
+
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if len(nums) == k:
+            return sum(nums)/k
+
+        # nums[:k] -> from 0 to k-1
+        curr = sum(x for x in nums[:k])
+        Max = curr
+        for i in range(k, len(nums)):
+            curr = curr+nums[i]-nums[i-k]
+            Max = max(Max, curr)
+        return Max/k
