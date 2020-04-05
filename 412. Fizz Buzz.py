83d7fc982d57e9c1d4b1af61a60a37fb4d75c593
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-04-04 21:46:57 -0400

    Upload 412

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,46 @@
+# Naive approach
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+
+# Hashing
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        # ans list
+        ans = []
+
+        # Dictionary to store all fizzbuzz mappings
+        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
+
+        for num in range(1,n+1):
+
+            num_ans_str = ""
+
+            for key in fizz_buzz_dict.keys():
+
+                # If the num is divisible by key,
+                # then add the corresponding string mapping to current num_ans_str
+                if num % key == 0:
+                    num_ans_str += fizz_buzz_dict[key]
+
+            if not num_ans_str:
+                num_ans_str = str(num)
+
+            # Append the current answer str to the ans list
+            ans.append(num_ans_str)
+
+        return ans
