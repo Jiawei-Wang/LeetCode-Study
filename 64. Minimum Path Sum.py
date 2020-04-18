fdea5a546634b6bcb5913f7f523353b2b6cc5d51
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-04-18 12:48:09 -0400

    Upload 64

--- 64. Minimum Path Sum.py
+++ 64. Minimum Path Sum.py
@@ -13,3 +13,20 @@ class Solution:
             for j in range(1, column):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         return grid[-1][-1]
+
+
+        # recursion (超时)
+        row = len(grid)
+        column = len(grid[0])
+
+        def dp(i,j):
+            if i != 0 and j != 0:
+                return grid[i][j]+ min(dp(i-1,j), dp(i,j-1))
+            elif i == 0 and j == 0:
+                return grid[0][0]
+            elif i == 0:
+                return grid[i][j]+ dp(i,j-1)
+            elif j == 0:
+                return grid[i][j] + dp(i-1,j)
+
+        return dp(row-1,column-1)
