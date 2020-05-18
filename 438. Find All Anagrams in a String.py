cad4529c6a6ec810f823f3d260c26800b1eec83b
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-05-17 22:25:05 -0400

    Upload 438

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -9,3 +9,25 @@ class Solution:
             if sorted(s[i:len(p)+i]) == new:
                 ans.append(i)
         return ans
+
+
+# 优化版本，主要优化点在于：不用每次都重新sort一个substring，只需要对dictionary进行很小的维护即可
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p)-1])
+        for i in range(len(p)-1,len(s)):
+            sCounter[s[i]] += 1   # include a new char in the window
+            if sCounter == pCounter:    # This step is O(1), since there are at most 26 English letters
+                res.append(i-len(p)+1)   # append the starting index
+            sCounter[s[i-len(p)+1]] -= 1   # decrease the count of oldest char in the window
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]]   # remove the count if it is 0
+        return res
