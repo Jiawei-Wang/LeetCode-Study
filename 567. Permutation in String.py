f83fa3606000257a8b067e777380a94720d4f1d0
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-05-18 22:16:12 -0400

    Upload 567

---
+++ 567. Permutation in String.py
@@ -0,0 +1,13 @@
+# 尝试使用438的解法：移动窗口
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        d1, d2 = Counter(s1), Counter(s2[:len(s1)])
+        for start in range(len(s1), len(s2)):
+            if d1 == d2: return True
+            d2[s2[start]] += 1
+            d2[s2[start-len(s1)]] -= 1
+            if d2[s2[start-len(s1)]] == 0:
+                del d2[s2[start-len(s1)]]
+        return d1 == d2
