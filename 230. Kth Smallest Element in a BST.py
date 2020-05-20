0f10f283bbd1896bf53f080b20e8450f3f6a6855
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-05-20 14:46:35 -0400

    Upload 230

---
+++ 230. Kth Smallest Element in a BST.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+
+        # 思路：BST中每个元素的位置均固定，所以DFS就可以找到第K个元素
+        def inorder(r):
+            # 将每个元素的值放进一个list中
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+
+        return inorder(root)[k - 1]
