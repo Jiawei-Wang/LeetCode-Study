6d80253a85dcee1e78ecc27f74b4dbbb088ce768
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-07-17 14:59:14 -0400

    Finish java s123 python s12

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -40,6 +40,23 @@ class Solution {
     }
 }
 
+/*
+在python中跑testcase: nums = [3,3], target = 6 时发现两个问题：
+1. 如何解决hashmap里每个key只能保存一个value的问题
+2. 为什么python中出现了这个testcase跑不过的问题
+
+1. 答：
+   这个问题无法解决，java的hashmap和python的dictionary中，每个key都只能存一个value，新的value会替换旧的value，所以nums数组中出现多次的数字，
+   只会保留最后一次出现的index。
+   其次为什么在java中虽然每个key（元素）只保存了一个value（该元素最后一次出现的index），但这个solution通过了所有的testcase：
+   因为首先index相同这个问题只会在以下情况下出现：即当前数字 = 1/2 target且nums中该数字只出现一次，如果出现多次的话就没有此问题，
+   因为遍历时找到的是第一次出现的index，而hashmap中保存的是最后一次出现的index，两者不相同。
+
+2. 答：
+   因为原代码中遍历查询使用的是 for i in nums 而不是 for i in range(len(nums))，而第一句代码中的 i ，如果查询它的index，python会给出 “i”
+   这个值第一次出现时的index，比如 [3,1,3] 中，如果查询每个 i，给出的index为：0，1，0 而非 0，1，2
+*/
+
 
 // solution 3: one pass hashing，遍历所有元素，如果hashmap中有complement，立刻返回，如果没有，将这个元素放入然后前进至下一个元素
 // Time: n
--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -6,3 +6,23 @@ class Solution:
                 if nums[i]+nums[j] == target:
                     return [i,j]
         return None
+
+
+# solution 2: hashmap
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # use dictionary as hashmap
+        # https://www.w3schools.com/python/python_dictionaries.asp
+        Map = {}
+        for i in range(0, len(nums)):
+            Map[nums[i]] = i
+        # search for complement
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            # 记得检查两个数的下标是否相同
+            if complement in Map and Map[complement] != i:
+                return [i, Map[complement]]
+        return None
+
+
+# solution 3: hashmap one pass
