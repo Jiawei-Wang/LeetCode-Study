bb58fe4e67dacc66e49b60b76997d7edd26956c0
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-07-22 13:11:14 -0400

    Finish 509

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,19 @@
+# iteration
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0: return 0
+        if N == 1: return 1
+        first = 0
+        second = 1
+        for i in range(2, N+1):
+            ans = first + second
+            first = second
+            second = ans
+        return ans
+
+# recursion
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0: return 0;
+        if N == 1: return 1;
+        return self.fib(N-1) + self.fib(N-2)
