bbe6b4d89b5c7528ac6388913aca3fd84e7961a4
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-07-22 15:57:00 -0400

    Finish 283 java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -29,3 +29,42 @@ class Solution {
         */
     }
 }
+
+
+// 解法2：将非0元素放置在前，然后尾部用0填充满
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non 0 element we found.
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // a = b++: 先 a = b；再 b += 1
+                // a = ++b: 先 b += 1；再 a = b
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        // After we have finished processing new elements,
+        // all the non-zero elements are already at beginning of array.
+        // We just need to fill remaining array with 0's.
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
+
+
+// 解法3：详见python版解析
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[zero];
+                nums[zero] = temp;
+                zero += 1;
+            }
+        }
+    }
+}
