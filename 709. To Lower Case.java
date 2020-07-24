8c8cb333c662519e78cb4a540ff0fde43b3ba0c6
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-07-23 22:23:31 -0400

    Finish 709 s1

---
+++ 709. To Lower Case.java
@@ -0,0 +1,7 @@
+// solution1: liberary
+// toLowerCase() method converts a string to lower case letters
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
---
+++ 709. To Lower Case.py
@@ -0,0 +1,4 @@
+# solution1: liberary
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
