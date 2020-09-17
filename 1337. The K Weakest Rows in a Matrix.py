95d7df8809116d789cb3698ab313ae1c12d41c66
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-09-17 00:37:22 -0400

    1337 python

--- 1337. The K Weakest Rows in a Matrix.py
+++ 1337. The K Weakest Rows in a Matrix.py
@@ -1,7 +1,22 @@
 # 读题理解:
-# 1. 题目要求: 将所有的row按照 1 的数量进行从小到大排序, 输出第 k 的row的下标
+# 1. 题目要求: 将所有的row按照 1 的数量进行从小到大排序, 输出前 k 个row的下标
 # 2. 限制: 1. 每个row的所有 1 都在前面, 0 在后面; 2. 当两个row的 1 的数量相同时, 下标小者顺序在前
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        # 先把每一row的 1 的数量找出来, 放进一个新的
+        # i是每row的下标, g是row这个array本身
+        # S是一个2d array, 每一行是 [1的数量, row的下标]
+        S = [[sum(g),i] for i,g in enumerate(mat)]
+        
+        # 从小到大排列
+        R = sorted(S)
+        
+        # 输出下标组成的array
+        return [r[1] for r in R[:k]]
+
+
+# one line with heap
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        # nsmallest(k, iterable, key = fun) :- This function is used to return the k smallest elements from the iterable specified and satisfying the key if mentioned
+        return [r[1] for r in heapq.nsmallest(k,[[sum(g),i] for i,g in enumerate(mat)])]
