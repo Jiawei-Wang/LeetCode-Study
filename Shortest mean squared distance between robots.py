6219c2d5225d4bdb5b8a70433806e0c10bbfa49f
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-10-05 23:03:14 -0400

    Start 2 questions

---
+++ Shortest mean squared distance between robots.py
@@ -0,0 +1,13 @@
+"""
+Amazon OA2 2020
+fing shortest distance between robots
+https://leetcode.com/discuss/interview-question/821708/amazon-oa-shortes
+https://blog.csdn.net/Jin_Kwok/article/details/82350019
+input:
+    numRobots: an integer representing the number of robots
+    positionX: a list of integers representing the x coordinates of the robot's position in meters
+    positionY: a list of integers representing the y coordinates of the robot's position in meters
+output:
+    return an integer representing the squared shortest distance between the pairs of robots
+"""
+
