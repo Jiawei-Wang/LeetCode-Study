59c40c3024b6c590e818f53ba69907da50e71ad2
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-10-10 21:10:25 -0400

    128 s12

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -26,3 +26,26 @@ class Solution:
                 count = 1
         ans = max(count, ans)
         return ans
+
+
+# 解法2: HashSet and Intelligent Sequence Building
+# N
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # list to set: O(n)
+        num_set = set(nums)
+
+        longest_streak = 0
+        for num in num_set:
+            # Query a set: O(1)
+            if num - 1 not in num_set: # if num is the start of a new streak
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
