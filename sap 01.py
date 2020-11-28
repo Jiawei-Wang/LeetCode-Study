68eaf87e298eacf09deaa017d099af105e16bdce
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-11-27 23:13:35 -0500

    sap 01 s12

--- sap 01.py
+++ sap 01.py
@@ -3,15 +3,30 @@ https://leetcode.com/discuss/interview-question/882393/SAP-or-2020
 https://leetcode.com/discuss/interview-question/421553/Interview-Question-SAP
 """
 
-# brute force
+# bulit-in 
+# time complexity: n^2
 class Solution:
     def move(self, n, index, identity):
         ans = []
         for i in range(n):
             curr = identity[i]
             pos = index[i]
-            ans.insert(pos, curr)
+            ans.insert(pos, curr) # insert is linear complexity
         return ans
+
+# brute force
+class Solution2:
+    def move(self, n, index, identity):
+        ans = []
+        for i in range(n):
+            curr = identity[i]
+            pos = index[i]
+            ans.append(curr)
+            for j in range(len(ans)-1, pos-1, -1):
+                ans[j] = ans[j-1]
+            ans[pos] = curr
+        return ans
+
 
 
 if __name__ == "__main__":
@@ -21,6 +36,8 @@ if __name__ == "__main__":
     identity = [0, 1, 2, 3, 4]
     case1 = Solution()
     print(case1.move(n, index, identity))
+    case12 = Solution2()
+    print(case12.move(n, index, identity))
 
     # test case 2: should return [2, 0, 1]
     n = 3
@@ -28,3 +45,5 @@ if __name__ == "__main__":
     identity = [0, 1, 2]
     case2 = Solution()
     print(case2.move(n, index, identity))
+    case22 = Solution2()
+    print(case22.move(n, index, identity))
