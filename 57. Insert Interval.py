1a4e813906a72403fe2cfd5afa0dd160ef37ae78
Jiawei Wang <wangjiawei_med@hotmail.com>
2022-06-07 02:18:14 -0700

    06-07-2022

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -1,3 +1,35 @@
+# 找到第一个interval，融合，然后将后面interval全部融合，如果没找到interval则找到插入位置并插入
+# 写这个答案的过程中出现了非常多的corner case报错，针对每个corner case去修改代码才通过
+class Solution:
+    def insert(self, l: List[List[int]], n: List[int]) -> List[List[int]]:
+        if not l: # corner case：注意空list直接加入
+            return [n]
+        length = len(l)
+        target = -1
+        for i in range(length):
+            # 两个元素重叠的判断：目标的头部或尾部在当前元素的范围内
+            if l[i][0] <= n[0] <= l[i][1] or l[i][0] <= n[1] <= l[i][1] or \
+            (l[i][0] <= n[0] and l[i][1] >= n[1]) or (l[i][0] >= n[0] and l[i][1] <= n[1]): # corner case: 注意还有一种重叠是一方被另一方全部包含
+                target = i
+                l[i][0] = min(l[i][0], n[0])
+                l[i][1] = max(l[i][1], n[1])
+                # 找到尾部最长延申位置
+                j = i + 1
+                while j < len(l) and l[i][1] >= l[j][0]: # corner case: 注意j越界
+                    l[i][1] = max(l[i][1], l[j][1])
+                    del l[j] # 因为该元素被删除，后面元素被往前挪，所以没必要 j+=1
+                break
+        # corner case: 注意如果遍历完都没发现interval，那么要找到在l中n应该插入的位置
+        if target == -1:
+            for i in range(length):
+                if l[i][0] > n[0]:
+                    l.insert(i, n)
+                    return l
+            l.append(n)
+        return l
+
+
+# 优化: 依旧是遍历，但是使用两个list，分别保存前面和后面不和newInterval相交的元素
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         s, e = newInterval[0], newInterval[1]
@@ -8,14 +40,15 @@ class Solution:
         # 2.元素头 newInterval头 元素尾 newInterval尾
         # 3.元素头 newInterval头 newInterval尾 元素尾
         # 4.newInterval头 元素头 newInterval尾 元素尾
-        # 5.newInterval头 newInterval尾 元素尾
+        # 5.newInterval头 newInterval尾 元素头 元素尾
         # 1和5不需要进行操作
         # 2、3、4需要将两者融合
+        
         for i in intervals:
             if i[1] < s: # 1.
                 left.append(i)
             elif i[0] > e: # 5.
-                right.append(i)
+                right.append(i) # 第一解中漏掉的细节：因为intervals中的元素本身不相交，所以newInterval的尾部覆盖的元素，在更新后不会和下一个元素相交
             else: 
                 s = min(s, i[0])
                 e = max(e, i[1])
--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -7,4 +7,23 @@ class Solution:
         return sorted(nums)
 
 
-# 
+# time n space n
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = deque([])
+        i = 0
+        j = len(nums)-1
+        while i <= j:
+            left = nums[i] * nums[i]
+            right = nums[j] * nums[j]
+            # 不用考虑正负问题，因为一个数字的平方肯定不为负
+            if left > right:
+                ans.appendleft(left)
+                i += 1
+            else:
+                ans.appendleft(right)
+                j -= 1
+        return ans
+
+
+# 不用deque的方法：初始化一个list，所有元素均为0，然后同样用two pointer，每次找到位置然后更新
