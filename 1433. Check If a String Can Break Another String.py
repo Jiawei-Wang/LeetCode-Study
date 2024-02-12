d93f764dee8e7978bfdf094aafa2b23058b4e89e
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-02-12 14:58:31 -0500

    1433

--- 1433. Check If a String Can Break Another String.py
+++ 1433. Check If a String Can Break Another String.py
@@ -10,3 +10,40 @@ class Solution:
             if big[i] < small[i]:
                 return False
         return True
+
+
+class Solution:
+    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        count1 = [0] * 26  # Array to count occurrences of characters in s1
+        count2 = [0] * 26  # Array to count occurrences of characters in s2
+
+        # Count occurrences of characters in s1
+        for char in s1:
+            count1[ord(char) - ord('a')] += 1
+
+        # Count occurrences of characters in s2
+        for char in s2:
+            count2[ord(char) - ord('a')] += 1
+
+        # Variables to track if s1 can break s2 and vice versa
+        can_break_s1 = True
+        can_break_s2 = True
+
+        # Check if s1 can break s2
+        diff = 0
+        for i in range(26):
+            diff += count1[i] - count2[i]
+            if diff < 0:
+                can_break_s1 = False
+                break
+
+        # Check if s2 can break s1
+        diff = 0
+        for i in range(26):
+            diff += count2[i] - count1[i]
+            if diff < 0:
+                can_break_s2 = False
+                break
+
+        # Return True if either s1 can break s2 or s2 can break s1
+        return can_break_s1 or can_break_s2
