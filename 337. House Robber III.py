91891fd4aa94c98dd728758d08057c7be5d8e734
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-02-19 14:47:41 -0500

    337

--- 337. House Robber III.py
+++ 337. House Robber III.py
@@ -19,4 +19,42 @@ class Solution:
             money += self.rob(root.right.left) + self.rob(root.right.right)
         
         return max(root.val + money, self.rob(root.left) + self.rob(root.right))
+
+
+# DP
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dp(root, hashmap):
+            if not root:
+                return 0
+            if root in hashmap:
+                return hashmap.get(root) # use existing result instead of calculating again
+            money = 0
+            if root.left:
+                money += dp(root.left.left, hashmap) + dp(root.left.right, hashmap)
+            if root.right:
+                money += dp(root.right.left, hashmap) + dp(root.right.right, hashmap)
+            money = max(money + root.val, dp(root.left, hashmap) + dp(root.right, hashmap))
+            hashmap[root] = money # put calculated result into hashmap
+            return money
+
+        hashmap = dict() # node, integer
+        return dp(root, hashmap)
+
+
+# better DP: 
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def betterDP(root):
+            if not root:
+                return [0, 0]
+            left = betterDP(root.left)
+            right = betterDP(root.right)
+            res = [0, 0]
+            res[0] = max(left[0], left[1]) + max(right[0], right[1]) 
+            res[1] = root.val + left[0] + right[0]
+            return res
+        res = betterDP(root)
+        return max(res[0], res[1])
+
         
