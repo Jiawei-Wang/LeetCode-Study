104dab2ff6305f0e0c3ba1912f9ce77b29f39d56
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-08 00:52:28 -0500

    45

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -1,23 +1,32 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         length = len(nums)
-        jump = [0] + [float('inf')] * (length-1)
+        dp = [0] + [float('inf')] * (length-1)
         for i in range(length):
-            forward = nums[i]
-            for j in range(1, forward+1):
-                if i+j < length and jump[i+j] == float('inf'):
-                    jump[i+j] = jump[i]+1
-        return jump[-1]        
+            coverage = nums[i]
+            for j in range(i+1, i+coverage+1):
+                if j < length:
+                    dp[j] = min(dp[i]+1, dp[j]) # find minimum steps to jump to j
+        return dp[-1]
 
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jump = 0
+        jump = 0 
         cur_end = 0
         cur_farthest = 0
-        for i in range(len(nums)-1):
-            cur_farthest = max(cur_farthest, i + nums[i])
-            if i == cur_end:
+        for i in range(len(nums)-1): # when we start from a position 
+            cur_farthest = max(cur_farthest, i + nums[i]) # we first update the farthest position we can reach
+            if i == cur_end: # we have reached the end of this jump
                 jump += 1
-                cur_end = cur_farthest
+                cur_end = cur_farthest # we find the range of next jump
         return jump
+        
+"""
+example: for [2, 3, 0, 1, 4]
+1. we start from 2, farthest will be index=2, 0-th jump reaches end, update end to 2, jump += 1
+2. we move on to 3, farthest will be index=5
+3. we move on to 0, farthest is still index=5, current jump reaches end, update end to 5, jump += 1
+4. we move on to 1, farthest is still index=5
+5. return jump value
+"""
