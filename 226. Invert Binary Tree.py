5d349a07e6a34cdd130d42d7b9647865d297a85f
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-09 12:45:26 -0500

    226

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -67,3 +67,25 @@ class Solution:
             if node.right:
                 queue.append(node.right)
         return root
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        def switch(node):
+            node.left, node.right = node.right, node.left
+            if node.left:
+                switch(node.left)
+            if node.right:
+                switch(node.right)
+        
+        switch(root)
+        return root
