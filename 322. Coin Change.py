82e3962a0927fc61c455d984ba3aab742593cdff
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-08 20:54:39 -0500

    322

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -10,3 +10,17 @@ class Solution:
         if dp[-1] == float('inf'): # 如果没有任何一个dp[i-coin]可以走到dp[i]，dp[i]在遍历结束时即为 inf
             return -1
         return dp[-1]
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # no coin is worth 0 so we need a starting point
+        # this covers the corner case: if amount is 0 we will return 0
+        dp = [0] + [float('inf')] * amount 
+
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+        
+        return dp[-1] if dp[-1] != float('inf') else -1
