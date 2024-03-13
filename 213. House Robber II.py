450b793097488aa5e04bff9005456131cd4dc623
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-12 20:42:00 -0400

    213

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -1,18 +1,21 @@
+# two loops: rob first house and not rob first house
+# rob first house = not rob last house
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # 两次分别计算rob第一家和不rob第一家的结果（rob第一家 = 不rob最后一家）
         if not nums:
             return 0
         if len(nums) == 1:
             return nums[0]
         
+        # first loop
         left = 0
-        right = 0
+        right = 0 # we only need two variables since we don't care about house[1] anymore once we reach house[4]
         for i in range(len(nums)-1):
             tmp = left
-            left = max(right+nums[i], left)
+            left = max(right+nums[i], left) # same as House Robber 1: dp[i] = max(dp[i-2]+nums[i], dp[i-1]) 
             right = tmp
         
+        # second loop
         left2 = 0
         right2 = 0
         for i in range(1, len(nums)):
@@ -20,4 +23,4 @@ class Solution:
             left2 = max(right2+nums[i], left2)
             right2 = tmp
         
-        return left if left >= left2 else left2
+        return max(left, left2)
