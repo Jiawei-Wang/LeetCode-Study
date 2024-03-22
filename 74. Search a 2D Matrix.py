3340a65971f035858e7181082f3001a746165b12
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-22 16:38:43 -0400

    74

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,32 +1,92 @@
+# two step brute force
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # binary search + n*m extra space
-        nums = []
-        for i in matrix:
-            nums += i
-        l, r = 0, len(nums)-1
-        while l <= r:
-            m = l + (r-l)//2
-            if nums[m] > target:
-                r = m-1
-            elif nums[m] < target:
-                l = m+1
-            else:
+        row = len(matrix)
+        col = len(matrix[0])
+        
+        # handle corner case: target out of matrix range
+        if target < matrix[0][0] or target > matrix[row-1][col-1]:
+            return False
+
+        # now we know target is within the matrix range
+        # first find the target row (we 100% will find an index)
+        row_list = [matrix[i][0] for i in range(row)] # list of first element of each row
+        low_row = 0
+        high_row = row - 1
+        while low_row < high_row:
+            mid_row = low_row + (high_row - low_row)//2 # lower bound
+
+            # handle corner case: we find target 
+            if target == row_list[low_row] or target == row_list[high_row] or target == row_list[mid_row]:
                 return True
-        return False
+            elif row_list[mid_row] > target: 
+                high_row = mid_row
+            else:
+                low_row = mid_row
+            
+            if low_row == high_row - 1: # we find two protential rows
+                if target > row_list[high_row]:
+                    low_row = high_row 
+                break 
+
+        # now we know target is within low_rol 
+        # second find the target col (we may not find an index)
+        col_list = [matrix[low_row][j] for j in range(col)]
+        low_col = 0
+        high_col = col - 1
+        while low_col < high_col:
+            mid_col = low_col + (high_col - low_col)//2
+
+            # handle corner case: we find target
+            if target == col_list[low_col] or target == col_list[high_col] or target == col_list[mid_col]:
+                return True
+            elif col_list[mid_col] > target:
+                high_col = mid_col
+            else:
+                low_col = mid_col
+            
+            if low_col == high_col - 1: # same as row, we have two protential cols
+                if target == matrix[low_row][low_col] or target == matrix[low_row][high_col]:
+                    return True
+                return False
+
+        return target == matrix[low_row][low_col] 
 
 
+# change to 1D binary search
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # 1 extra space: basically just calculate the [i, j] instead of making a new list
-        if not matrix: return False
-        start = 0
+        array = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                array.append(matrix[i][j])
+
+        length = len(array)
+        low = 0
+        high = length - 1
+        
+        while low < high:
+            mid = low + (high-low)//2
+            if array[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        return array[low] == target
+
+
+# O(1) extra space: search in place instead of making a new array
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows = len(matrix)
         cols = len(matrix[0])
+
+        start = 0
         end = rows*cols-1
-        while start <= end:
-            mid = (start+end) // 2
-            if matrix[mid//cols][mid%cols] == target: return True
+        
+        while start <= end: # when start = end, we still need to do calculation one more loop
+            mid = end + (start - end) // 2
+            if matrix[mid//cols][mid%cols] == target: 
+                return True
             if matrix[mid//cols][mid%cols] < target:
                 start = mid + 1
             else:
