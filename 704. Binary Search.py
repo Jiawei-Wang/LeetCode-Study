4cc5894011e248a849fc931e1d45c88c4b26a3b8
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-03-22 15:29:19 -0400

    704

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,13 +1,33 @@
+"""
+2024:
+key is to NOT miss any element
+1. pick range size: all elements
+2. pick stop sign: low = high, so while statement should be: low < high
+3. pick mid: lower mid, so it should be < target to prevent infinite loop
+4. pick new low/high: < target so low = mid + 1
+5. return when loop exits: return low
+"""
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
-        
-        while l <= r: # 最后一步可能只剩下一个元素，所以要用 <= 而不是 <
-            m = l + (r-l)//2 # 防溢出
-            if nums[m] > target:
-                r = m-1 # m 本身已被排除，所以只需要从m的左边第一个元素开始继续寻找即可
-            elif nums[m] < target:
-                l = m + 1
+        low = 0
+        high = len(nums)-1
+        while low < high:
+            mid = low + (high-low)//2
+            if nums[mid] < target:
+                low = mid + 1
             else:
-                return m
-        return -1
+                high = mid
+        return low if nums[low] == target else -1\
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+        while low < high:
+            mid = low + (high-low+1)//2 # if we want to use higher mid
+            if nums[mid] > target: # we need to use this to prevent missing any element and getting into infinite loop
+                high = mid - 1
+            else:
+                low = mid
+        return low if nums[low] == target else -1
