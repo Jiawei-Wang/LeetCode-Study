76e05a7adbff40801cf47ebd6cfa150f629954c0
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-09 13:37:31 -0400

    130

--- 130. Surrounded Regions.py
+++ 130. Surrounded Regions.py
@@ -34,3 +34,46 @@ class Solution:
             for c in range(col):
                 if board[r][c] == 'T':
                     board[r][c] = "O"
+
+
+# 2024
+# start from border
+# find all regions connected to border
+# leave them be and mark everything else "X"
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def find_region(row, col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != 'O':
+                return
+            
+            # Mark the current cell as visited
+            board[row][col] = '#'
+            
+            # Directions for neighbor exploration
+            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            
+            # Explore neighbors
+            for dr, dc in directions:
+                find_region(row + dr, col + dc)
+
+        
+        if not board:
+            return
+        rows, cols = len(board), len(board[0])
+        
+        # Mark regions connected to boundaries
+        for i in range(rows):
+            for j in range(cols):
+                if (i == 0 or i == rows - 1 or j == 0 or j == cols - 1) and board[i][j] == 'O':
+                    find_region(i, j)
+        
+        # Flip the marked 'O's to 'X's and revert '#' back to 'O's
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == '#':
+                    board[i][j] = 'O'
