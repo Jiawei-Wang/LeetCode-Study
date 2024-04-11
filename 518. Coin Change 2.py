2ff9211711baa7fbb272ebaa952192d92d05b42d
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-10 21:39:45 -0400

    518

--- 518. Coin Change 2.py
+++ 518. Coin Change 2.py
@@ -1,3 +1,59 @@
+# 2024
+"""
+2d dp:
+
+1. every row is a coin
+2. every column is an amount, last position is the target amount
+3. at every position, we can choose any one coin from coins
+4. to prevent duplicate: [1,1,2] is the same as [2,1,1] or [1,2,1]:
+    we do not allow picking smaller coins: if 2 is picked, following coins cannot be smaller than 2
+    so [2,1,1] and [1,2,1] are no more
+
+for example: amount = 5, coins = [1,2,5]
+grid looks like 
+        5   4   3   2   1   0
+    1                       1 <- only one way to sum up to 0
+    2                       1 <- how many ways can we sum up to 0 using 2 and 5
+    5                       1 <- how many ways can we sum up to 0 using only 5 (if we pick 5 we cannot pick other coins)
+
+computation order: right to left, bottom to top
+how to do it:
+to get to 1 using only 5s, we need to check -4, since -4 is out of bound, there is 0 way to do it
+to get to 1 using 2s and 5s, we need to check -1 and 5s position, there is 0 + 0 way to do it
+to get to 1 using 1s, 2s, and 5s, we need to check 0 and 2s position, there is 1 + 0 way to do it
+
+so this answer can actually turn into 1d dp by only keeping two arrays
+"""
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # create 2d array
+        dp = []
+        for i in range(len(coins)):
+            dp.append([0]*amount+[1]) # last column is filled out
+
+        # fill out first row 
+        last_row = len(coins) - 1
+        coin = coins[last_row]
+        for column in range(amount-1, -1, -1):
+            current = amount - column
+            if current % coin == 0:
+                dp[last_row][column] = 1
+            else:
+                dp[last_row][column] = 0
+
+        # rest of rows
+        for row in range(len(coins)-2, -1, -1):
+            for column in range(amount-1, -1, -1):
+                coin = coins[row]
+                current = amount - column
+                if current - coin < 0:
+                    dp[row][column] = dp[row+1][column]
+                else:
+                    dp[row][column] = dp[row][column + coin] + dp[row+1][column]
+                        
+        return dp[0][0]
+
+
 # 至少有一个coin
 # coins的数额永远为正数
 # amount不为负
