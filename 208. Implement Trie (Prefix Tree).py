97f8183f2ac1f92731519a15ec91e3648e3c2685
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-23 11:59:38 -0400

    208

--- 208. Implement Trie (Prefix Tree).py
+++ 208. Implement Trie (Prefix Tree).py
@@ -9,9 +9,11 @@
 树拥有多个node，每个node也拥有多个节点
 将一个node标记为末尾来表示从树根到它的一条路径为一个单词
 """
+
+# create a node class first
 class TrieNode:
     def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
+        self.children = collections.defaultdict(TrieNode) # a dict where default value (TrieNode) is given to key if there is none 
         self.is_word = False
 
 class Trie:
@@ -21,7 +23,7 @@ class Trie:
     def insert(self, word):
         current = self.root
         for letter in word:
-            current = current.children[letter]
+            current = current.children[letter] 
         current.is_word = True # 最后一个字母标记为结尾
 
     def search(self, word):
@@ -39,3 +41,18 @@ class Trie:
             if current is None:
                 return False
         return True # 和search的对比在于：同样都使用input string在树里往下走，走不到最后就返回False，但不同之处在于如果走到最后，不需要查看此char的is_word属性
+
+
+"""
+understand
+    for letter in word:
+        current = current.children[letter]
+1. current.children is a ictionary: key = letter, value = TrieNode object
+2. current.children[letter] accesses the value for the letter key in children dictionary
+3. if key exists, TrieNode object is returned, if not, TrieNode object is created for the key
+for example: insert "abc"
+1. current points to dummy head
+2. dummy.children[a] = a new TrieNode (a is the key in dict, TrieNode is the value)
+3. current points to this new TrieNode (accessed by giving a as key)
+4. do the same thing for b in a.children, and c in b.children
+"""
