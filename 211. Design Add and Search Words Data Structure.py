7c8365fc715dee36dc33abc51a217ec9463568aa
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-23 12:32:56 -0400

    211

--- 211. Design Add and Search Words Data Structure.py
+++ 211. Design Add and Search Words Data Structure.py
@@ -39,3 +39,47 @@ class WordDictionary:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
+
+# 2024
+from collections import defaultdict
+class Node:
+    def __init__(self):
+        self.children = defaultdict(Node)
+        self.is_end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        current = self.root
+        for char in word:
+            current = current.children[char]
+        current.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(pivot, node):
+            current = node
+            for i in range(pivot, len(word)):
+                char = word[i]
+                if char == ".":
+                    for child in current.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if char not in current.children:
+                        return False
+                    current = current.children[char]
+            return current.is_end
+
+        return dfs(0, self.root)
+
+"""
+understanding: compare to 208. Implement Trie
+1. if there is no dot, just do the same as 208
+2. if there is dot: we do a dfs everytime a new dot shows up
+3. in dfs: instead of searching one node, we need to search all possible nodes
+4. two things are needed: node, and next char in input string
+"""
