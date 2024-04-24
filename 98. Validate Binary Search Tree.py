569670eb4249c40e45d0220a339ca9b41c783c3d
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-24 17:54:02 -0400

    98

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -1,21 +1,63 @@
-# recursion，检查每个subtree是否符合left node < node < right node 
+"""
+below solution is incorrect, it will fail on this: 
+                      5
+                4          6
+                         3   7
+and return True
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root.left and not root.right: # if node has no child
+            return True
+        elif root.left and root.right: # if node has both children
+            if not (root.left.val < root.val < root.right.val):
+                return False
+            else:
+                return self.isValidBST(root.left) and self.isValidBST(root.right)
+        elif root.left and not root.right: # if node has left child
+            if not root.left.val < root.val:
+                return False
+            else:
+                return self.isValidBST(root.left)
+        else: # if node has right child
+            if not root.right.val > root.val:
+                return False
+            else:
+                return self.isValidBST(root.right)
+"""
+
+
+"""
+correct solution: find the available value range for all nodes
+for example: 
+                      5
+                4          6
+                         3   7
+for root: -inf < 5 < inf
+for 4: -inf < 4 < 5
+for 6: 5 < 6 < inf
+for 7: 6 < 7 < inf
+for 3: 5 < 3 < 6: return False
+"""
 class Solution:
     def isValidBST(self, root):
-        return self.check_bst(root, float("-inf"), float("inf"))
+        return self.check_bst(root, float("-inf"), float("inf")) 
 
-    def check_bst(self, node, left, right):
+    def check_bst(self, node, left, right): 
         if not node:
             return True
 
         if not left < node.val < right:
             return False
 
-        return (self.check_bst(node.left, left, node.val) 
+        # if node exists and node.val is within range
+        return (self.check_bst(node.left, left, node.val)  
                 and self.check_bst(node.right, node.val, right))
     
     '''
-    初始时left = -inf，right = inf
+    left and right are NOT child node values, they are available value range
+    初始时left = -inf，right = inf (root doesn't have value range)
     每次进入下一层时，根据左右subtree，将当前node的值更新为新的限制条件
     比如root的左子node的限制为：-inf < value < root.val
-    假设此node = 5，则它的右子node的限制为 5 < value < root.val
+    假设root = 10, 此node = 5，则它的右子node的限制为 5 < value < 10
     '''
