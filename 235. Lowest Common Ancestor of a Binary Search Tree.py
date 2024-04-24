b52c2606b365270f5eb7a1f3ba0e5317d4e39c78
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-24 11:56:23 -0400

    235

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -26,3 +26,23 @@ class Solution:
         if p.val > root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
         return root
+
+
+# 2024
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # if p and q are not on the same side
+        if (root.val < p.val and root.val > q.val) or (root.val > p.val and root.val < q.val):
+            return root
+        
+        # if p and q are on the same side
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        # if pointer is already pointing at p or q
+        elif root.val == p.val:
+            return p
+        elif root.val == q.val:
+            return q
