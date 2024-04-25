ee51cc2b4dc66f6fe582246b4b3d2876b2cd3681
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-25 17:13:39 -0400

    73

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -32,3 +32,39 @@ class Solution:
 而对于第一行而言，遍历时是最先经过的，所以当进入其他行时，再用第一行来储存每一列是否为0，就不会造成冲突
 """
         
+
+# 2024
+# we can use elements in first col to mark if one row should be all 0s
+# then we will lose information about if first col should be all 0s
+# if any row should be all 0s, there will be a 0 on the first col
+# so we need an extra variable to hold first col information
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        col0 = 1 
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        for i in range(rows): # we go through every row
+            if matrix[i][0] == 0: # if any element in the first col is 0
+                col0 = 0 # we use an extra variable to mark first col as 0
+            # go through every other element in this row
+            # we don't care about matrix[i][0] because:
+            # 1. if it is 0: it will stay 0 (whole row is marked)
+            # 2. if it is not 0: it only becomes 0 when another element on this row is 0
+            for j in range(1, cols): 
+                if matrix[i][j] == 0: # if any element (row i col j) is 0
+                    # we use first element in this row to mark row i as 0, first element in this col to mark col j as 0
+                    matrix[i][0] = matrix[0][j] = 0 
+            # this works for (using first row to store col inforamtion) because:
+            # we go into first row first matrix[0][j]
+            # 1. if it is 0: it will stay 0 (whole col is marked)
+            # 2. if it is not 0: it only becomes 0 when another element on this col is 0
+                    
+        # change the matrix based on marks
+        # we have to go backwards (for example if matrix[0][0] is 0, going forward will make the whole matrix 0)
+        for i in range(rows-1, -1, -1):
+            for j in range(cols-1, 0, -1): # first col is ignored
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if col0 == 0: # check if this element in first col needs to be updated
+                matrix[i][0] = 0
