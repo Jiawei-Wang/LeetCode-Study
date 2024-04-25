f99663a09dec2cafc99004e5ce556d38fdf81a39
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-25 16:46:09 -0400

    48

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -26,3 +26,39 @@ class Solution:
         # ~a = -a-1
 
 
+# 2024
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        length = len(matrix)
+        # if length is 4, a quarter is [0,0] to [1,1] with no center element in matrix
+        # if length is 5, a quarter is [0,0] to [1,1] with [2,2] being the center element in matrix
+        # if we select [0,0] to [2,2] as a quarter, some elements will be moved more than once
+        quarter_length = length//2
+        for i in range(quarter_length):
+            for j in range(quarter_length):
+                one   = matrix[i][j] 
+                two   = matrix[j][length-1-i]
+                three = matrix[length-1-i][length-1-j]
+                four  = matrix[length-1-j][i]
+                
+                matrix[i][j]                    = four
+                matrix[j][length-1-i]           = one
+                matrix[length-1-i][length-1-j]  = two
+                matrix[length-1-j][i]           = three
+        
+        # if length is odd: deal with remaining elements in center horizontal line and in center vertical line
+        if length % 2: 
+            for i in range(quarter_length):
+                one     = matrix[i][quarter_length]
+                two     = matrix[quarter_length][length-1-i]
+                three   = matrix[length-1-i][length-1-quarter_length]
+                four    = matrix[length-1-quarter_length][i]
+
+                matrix[i][quarter_length]                    = four
+                matrix[quarter_length][length-1-i]           = one
+                matrix[length-1-i][length-1-quarter_length]  = two
+                matrix[length-1-quarter_length][i]           = three
+
