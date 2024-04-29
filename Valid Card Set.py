927928b41f0ffbef36f4c5c38bd19425428cc82f
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-04-29 18:30:01 -0400

    2024 google phone screen

--- Valid Card Set.py
+++ Valid Card Set.py
@@ -16,10 +16,14 @@ OR
 """
 
 def is_valid_set(cards):
+    # if less than 3 cards
     length = len(cards)
     if length <= 2:
         return False
-        
+
+    existing = [False] * 13 # a list of existing cards: for example 3C will be at index = 2, JH will be at index = 10
+
+    # method to return index on existing based on the number of the card 
     def match_index(num):
         if num == "A":
             return 0
@@ -32,33 +36,43 @@ def is_valid_set(cards):
         else:
             return int(num)-1
         
+    # method to check if there is a gap on existing
     def is_consecutive(existing):
         for i in range(first_exist, last_exist+1):
             if existing[i] == False:
                 return False
         return True
     
+    # get first card
     target_shape = cards[0][-1]
     target_number = cards[0][:-1]
 
-    existing = [False] * 13
+    # update existing and create two pointers    
     first_index = match_index(target_number)
     existing[first_index] = True
     first_exist = first_index
     last_exist = first_index
     
+    # from the second card
     for index in range(1, length):
         card = cards[index]
         shape = card[-1]
         number = card[:-1]
         
+        # if any card has both different number and different shape than first card, it's False
         if shape != target_shape and number != target_number: 
             return False
+        
+        # if the card has same shape or same number
         else:
             idx = match_index(number)
             existing[idx] = True
-            first_exist = min(first_exist, idx)
+            first_exist = min(first_exist, idx) 
             last_exist = max(last_exist, idx)
+    
+    # for different number consecutive cards, first will return True
+    # for different number non consecutive cards, both will return False
+    # for same number different shape cards, second will return True
     return is_consecutive(existing) or first_exist == last_exist
 
             
