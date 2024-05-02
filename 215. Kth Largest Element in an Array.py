3cbe59ee60989addb1efc2364ac7f0a574ee3a85
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-02 15:15:13 -0400

    215

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -5,28 +5,20 @@ class Solution:
         return nums[len(nums)-k]
 
 
-# solution2: heapq module
+# solution2: heapq
 import heapq
-
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         heapq.heapify(nums)
         return heapq.nlargest(k, nums)[-1]
 
 
-# 06-12-2022
-# 给一个list，找到第k大的元素
-# 举例：[3,2,1,6,5,4], k = 2
-# 因为sort后list = [1,2,3,4,5,6]，所以第2大的元素是5
-
-# 暴力解：先sort再找
-
 # 维持一个大小为k的heap
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         h = [float('-inf')] * k
         heapq.heapify(h)
-        for i in nums:
-            heapq.heappushpop(h, i)
+        for num in nums:
+            heapq.heappushpop(h, num)
         ans = heapq.heappop(h)
         return ans
