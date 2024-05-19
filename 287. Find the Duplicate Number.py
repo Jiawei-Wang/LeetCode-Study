2f7bc0605e372dd9ada20970acba39fc1f168708
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-18 21:33:36 -0400

    287

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -94,9 +94,12 @@ Assume the distance from head to the start of the loop is x1
 the distance from the start of the loop to the point fast and slow meet is x2
 the distance from the point fast and slow meet to the start of the loop is x3
 
-What is the distance fast moved? 2. What is the distance slow moved? 3. And their relationship?
+What is the distance fast moved?
 x1 + x2 + x3 + x2
+What is the distance slow moved? 
 x1 + x2
+And their relationship?
 x1 + x2 + x3 + x2 = 2 (x1 + x2)
-Thus x1 = x3, that's the reason we reset fast to be 0.
+
+Thus x1 = x3, that's the reason we reset slow to be 0.
 """
