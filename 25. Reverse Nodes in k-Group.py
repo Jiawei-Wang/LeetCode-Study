92dfdf8e72d7a561c53593103b639c2b36a82696
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-19 13:54:58 -0400

    25

--- 25. Reverse Nodes in k-Group.py
+++ 25. Reverse Nodes in k-Group.py
@@ -5,26 +5,37 @@
 #         self.next = next
 
 # 将linked list每k个元素进行翻转，最后不足k个的部分保持原位
+# (node value cannot be changed, only node connections can be changed)
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # find the first segment
         curr = head
         count = 0
-        
-        # find the k+1 node
         while curr and count != k:
             curr = curr.next
             count += 1
-        
-        # if k+1 node is found
-        if count == k:
-            curr = self.reverseKGroup(curr, k) # reverse list with k+1 node as head
-            # head - head-pointer to direct part, 
-            # curr - head-pointer to reversed part;
-            while count > 0: # reverse current k-group
+        # now curr is point at the k+1 node (the starting node for the 2nd segment) 
+        # notice: this node can be None, if we only have first segment
+
+        # if we don't have enough nodes for first segment, we do nothing
+        # if we have enough nodes for first segment, we flip everything 
+        if count == k: 
+            # when we flip, we don't want to mess up the connection, so we flip the last segment first
+            # then the one before last one, ..., lastly the first segment
+            curr = self.reverseKGroup(curr, k) # so now curr is the new curr for the next segment
+            
+            while count > 0: # reverse current segment
+                # when we first enter this while loop: 
+                # head: first element in current segment
+                # curr: first element in next segment
+                # we let head connect to curr
+                # then let head become curr and head.next become new head
+                # then repeat
                 count -= 1
                 tmp = head.next
                 head.next = curr
                 curr = head
                 head = tmp
             head = curr
+            
         return head
