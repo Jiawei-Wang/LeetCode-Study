d8de0a68ef303a7a09c99539a7cf0c6eb5cfa8ef
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-19 13:08:27 -0400

    139

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -6,10 +6,9 @@
 # d[7] is True because there is "code" in the dictionary that ends at the 7th index of "leetcode" AND d[3] is True
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        d = [False] * len(s)    
+        dp = [False] * len(s)    
         for i in range(len(s)):
             for w in wordDict:
-                # 如果当前 d 的subarray：1.的substring能在wordDict中找到，2.且前面部分已经被证明True或者前面是整个array的头
-                if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
-                    d[i] = True
-        return d[-1]
+                if w == s[i-len(w)+1:i+1] and (dp[i-len(w)] or i-len(w) == -1):
+                    dp[i] = True
+        return dp[-1]
