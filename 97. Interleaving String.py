febda7a15ee154fc1b77f43aec20ef64db9e833f
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-20 10:28:50 -0400

    97

--- 97. Interleaving String.py
+++ 97. Interleaving String.py
@@ -4,7 +4,6 @@
 # 4.检查：语法是否有误，corner case and test case brain storm，手算一遍代码
 
 
-
 """
 思考：我们并不需要去考虑 abs(# of substrings(s1)) - # of substrings(s2)) <= 1的问题
      因为我们一直在轮流切换string，所以结尾时两个string的substring数量之差必然为0或者1
@@ -63,3 +62,123 @@ class Solution:
 上面答案是正确的，但是爆内存
 """
 
+
+# 2024
+"""
+translate the question into DFS on graph:
+1. question asks: to create s3 by switching between s1 and s2
+2. so we can have a 2d grid, with chars from s1 as row and chars from s2 as col
+3. we start from topleft and move either to the right or to the bottom
+    1) to the right: we take a char from s2
+    2) to the bottom: we take a cahr from s1
+4. if we can reach bottomright, s3 can be formed
+"""
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        r, c, l= len(s1), len(s2), len(s3)
+        if r + c != l:
+            return False
+
+        stack, visited = [(0, 0)], set((0, 0))
+
+        while stack:
+            x, y = stack.pop()
+            # for every position we are currently in, we have 3 options
+
+            # 1. if it's already the bottomright
+            # (only when x = r, y = c will x + y = l)
+            if x + y == l:
+                return True
+            
+            # 2. if we want to move down:
+            #   1) we are currently standing within bound
+            #   2) and this char matches the char we need
+            #   3) and the one below (regardless if it's within bound or out of bound) is new 
+            if x < r and s1[x] == s3[x+y] and (x+1, y) not in visited:
+                stack.append((x+1, y))
+                visited.add((x+1, y))
+
+            # 3. if we want to move to right
+            if y < c and s2[y] == s3[x+y] and (x, y+1) not in visited:
+                stack.append((x, y+1))
+                visited.add((x, y+1))
+
+            """
+            we want to move down != we can move down
+            1. we need check if we can move down
+            2. if we can move both down or to the right, we pick one
+            3. so we dfs to see if we can find one valid path
+            """
+
+        return False
+
+
+# DP 
+# O(m*n) space
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        r, c, l= len(s1), len(s2), len(s3)
+
+        if r+c != l:
+            return False
+        
+        dp = [[True for _ in range(c+1)] for _ in range(r+1)]
+        # dp[i][j] = True: we can create s3[0: i+j] by using s1[0:i] and s2[0:j]
+        # dp[0][0] = True
+
+        for i in range(1, r+1):
+            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+        for j in range(1, c+1):
+            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+        
+        for i in range(1, r+1):
+            for j in range(1, c+1):
+                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i-1+j]) or \
+                (dp[i][j-1] and s2[j-1] == s3[i-1+j])
+        return dp[-1][-1]
+
+
+# O(2*n) space
+# because when we go through the grid, we go row by row
+# so two rows are enough
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1, l2 = len(s1)+1, len(s2)+1
+        
+        if l1 + l2 != len(s3) + 2:
+            return False
+
+        pre = [True for _ in range(l2)]
+        for j in range(1, l2):
+            pre[j] = pre[j-1] and s2[j-1] == s3[j-1]
+
+        for i in range(1, l1):
+            # first create the new row
+            cur = [pre[0] and s1[i-1] == s3[i-1]] * l2
+            for j in range(1, l2):
+                # then calculate the new row
+                cur[j] = (cur[j-1] and s2[j-1] == s3[i+j-1]) or \
+                        (pre[j] and s1[i-1] == s3[i+j-1])
+            # last reassign the value
+            pre = cur[:]
+
+        return pre[-1]
+
+
+# O(n) space
+# because we use prev row to calculate curr row, and reassign curr to be prev
+# we can also just do the calculation in place on prev alone
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        r, c, l= len(s1), len(s2), len(s3)
+        if r+c != l:
+            return False
+        dp = [True for _ in range(c+1)] 
+        for j in range(1, c+1):
+            dp[j] = dp[j-1] and s2[j-1] == s3[j-1]
+
+        for i in range(1, r+1):
+            dp[0] = (dp[0] and s1[i-1] == s3[i-1])
+            for j in range(1, c+1):
+                dp[j] = (dp[j] and s1[i-1] == s3[i-1+j]) or (dp[j-1] and s2[j-1] == s3[i-1+j])
+        return dp[-1]
