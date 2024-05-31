c90e62e718e216e07a1422ad191ee6c178d16293
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-31 13:21:39 -0400

    560

--- 560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K.py
@@ -1,20 +1,5 @@
-# 为什么不能用sliding window：因为nums中的元素有正有负，而two pointer/sliding window的原则为：
-# 如果我知道大窗口的结果，就可以得到小窗口的结果，知道小窗口的结果也可以反推出大窗口的结果
-# 而这里的结果是完全随机的
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        count = collections.defaultdict(lambda: 0)
-        count[0] = 1
-        ans = 0
-        total = 0
-        for num in nums:
-            total += num
-            ans += count[total - k]
-            count[total] += 1
-        return ans
-
-
-# 2024
+# sliding window cannot be used in this question since num can be any integer 
+# so when right index finds a subarray, we don't know whether to move right index or left index
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
@@ -34,3 +19,17 @@ class Solution:
                 hashmap[total] += 1
         
         return count
+
+
+# improve upon above solution 
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = collections.defaultdict(lambda: 0)
+        count[0] = 1
+        ans = 0
+        total = 0
+        for num in nums:
+            total += num
+            ans += count[total - k]
+            count[total] += 1
+        return ans
