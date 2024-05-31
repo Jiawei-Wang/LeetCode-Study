a2ab52a659ccd0cfa74a15fd53667b3a7fc6273a
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-05-31 11:28:55 -0400

    724

---
+++ 724. Find Pivot Index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums) - nums[0]
+        for i in range(len(nums)):
+            if i == len(nums)-1:
+                return i if left == 0 else -1
+
+            if left == right:
+                return i
+            else:
+                left += nums[i]
+                right -= nums[i+1]
