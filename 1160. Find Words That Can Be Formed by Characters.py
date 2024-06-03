67e208e35deb2701fa05b55fd11e7cf3db158f57
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-06-03 09:36:22 -0400

    1160

--- 1160. Find Words That Can Be Formed by Characters.py
+++ 1160. Find Words That Can Be Formed by Characters.py
@@ -15,3 +15,24 @@ class Solution:
             if check(word):
                 ans += len(word)
         return ans
+
+
+# 2024
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        count = Counter(chars)
+
+        def contains(word):
+            w = Counter(word)
+            for key, value in w.items():
+                if key not in count:
+                    return False
+                elif value > count[key]:
+                    return False
+            return True
+
+        answer = 0
+        for word in words:
+            if contains(word):
+                answer += len(word)
+        return answer
