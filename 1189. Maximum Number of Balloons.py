16af206b6ae5f83824683462e7312ece6b8e517c
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-06-03 09:16:05 -0400

    1189

--- 1189. Maximum Number of Balloons.py
+++ 1189. Maximum Number of Balloons.py
@@ -16,3 +16,30 @@ class Solution:
         cnt = collections.Counter(text)
         cntBalloon = collections.Counter('balloon')
         return min([cnt[c] // cntBalloon[c] for c in cntBalloon])
+
+
+# 2024
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        """
+        balloon:
+        a: 1
+        b: 1
+        n: 1
+        l: 2
+        o: 2
+        """
+
+        hashmap = {"a":0, "b":0, "n":0, "l":0, "o":0}
+
+        for char in text:
+            if char in hashmap:
+                hashmap[char] += 1
+        
+        answer = float('inf')
+        for key, value in hashmap.items():
+            if key == "l" or key == "o":
+                answer = min(answer, value//2)
+            else:
+                answer = min(answer, value)
+        return answer
