cab37e8a496e0a8e85e88b68575d60576c2a66ea
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-05-03 10:01:27 -0400

    686

--- 686. Repeated String Match.py
+++ 686. Repeated String Match.py
@@ -15,3 +15,33 @@ class Solution:
                 return (i + j - 1) // len_a + 1
 
         return -1
+    
+
+# KMP
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        # Build the prefix table for KMP
+        prefTable = [0] * (len(b) + 1)  # 1-based indexing
+        sp, pp = 1, 0
+        while sp < len(b):
+            if b[pp] == b[sp] or pp == 0:
+                if b[pp] == b[sp]:
+                    pp += 1
+                else:
+                    pp = 0
+                sp += 1
+                prefTable[sp] = pp
+            else:
+                pp = prefTable[pp]
+
+        # Match b inside repeated a using KMP-like logic
+        i, j = 0, 0
+        while i < len(a):
+            while j < len(b) and a[(i + j) % len(a)] == b[j]:
+                j += 1
+            if j == len(b):
+                return (i + j - 1) // len(a) + 1
+            j = prefTable[j]
+            i += max(1, j - prefTable[j])
+
+        return -1
