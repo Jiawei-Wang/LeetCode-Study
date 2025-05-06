fc526fdf5a5933b48a546a38e00dddca6c85a886
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-05-05 21:27:39 -0400

    429

---
+++ 429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+from collections import deque
+from typing import List
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level_size = len(queue)
+            level = []
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+            result.append(level)
+        
+        return result
+
