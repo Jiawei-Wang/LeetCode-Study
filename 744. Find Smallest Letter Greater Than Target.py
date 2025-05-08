92aae8e4dd6a4271d9d111ee7f0c0ceb08e2aaee
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-05-08 10:32:18 -0400

    744

--- 744. Find Smallest Letter Greater Than Target.py
+++ 744. Find Smallest Letter Greater Than Target.py
@@ -5,3 +5,22 @@ class Solution:
             if char > target:
                 return char
         return letters[0]
+    
+
+# binary search
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if target >= letters[-1] or target < letters[0]:
+            return letters[0]
+        
+        low = 0
+        high = len(letters)-1
+        while low <= high:
+            mid = low + (high-low)//2
+            
+            if target >= letters[mid]: 
+                low = mid+1
+            else:
+                high = mid-1
+                
+        return letters[low]
