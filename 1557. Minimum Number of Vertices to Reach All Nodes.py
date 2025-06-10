3c68e916d8f95be8e48ee72ebe0fb43e30ae058a
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-06-10 18:10:59 -0400

    1557

---
+++ 1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -0,0 +1,4 @@
+# return nodes with no in-degree
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        return list(set(range(n)) - set(j for i, j in edges))
