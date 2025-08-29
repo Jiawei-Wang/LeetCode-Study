ee8a58f80df8ffbfb88e593295199cdd68507121
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-08-28 23:34:01 -0400

    1319

--- 1319. Number of Operations to Make Network Connected.py
+++ 1319. Number of Operations to Make Network Connected.py
@@ -33,3 +33,64 @@ class Solution:
         for a, b in connections:
             uf.union(a,b)
         return len(set(uf.find(i) for i in range(n))) - 1 # number of groups - 1
+
+
+# dfs
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+        
+        # create graph 
+        graph = defaultdict(list)
+        for a, b in connections:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        visited = [False] * n
+
+        def dfs(node):
+            stack = [node]
+            while stack:
+                cur = stack.pop()
+                if not visited[cur]:
+                    visited[cur] = True
+                    stack.extend(graph[cur])
+                    # list.extend(iterable) takes another iterable and adds each of its elements to the end
+                    # list.append(iterable) treats the whole iterable as a single element
+
+        count = 0
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                count += 1
+        return count - 1
+
+
+# bfs: switch from stack to queue
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+        graph = defaultdict(list)
+        for a, b in connections:
+            graph[a].append(b)
+            graph[b].append(a)
+        visited = [False] * n
+
+        def bfs(node):
+            q = deque([node])
+            visited[node] = True
+            while q:
+                cur = q.popleft()
+                for nei in graph[cur]:
+                    if not visited[nei]:
+                        visited[nei] = True
+                        q.append(nei)
+
+        count = 0
+        for i in range(n):
+            if not visited[i]:
+                bfs(i)
+                count += 1
+        return count - 1
