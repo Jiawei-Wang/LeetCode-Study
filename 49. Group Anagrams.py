6867b71579f32e64aee380918c7c2c7195e4b932
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-09-20 11:10:18 -0400

    49

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -82,3 +82,18 @@ class Solution:
             else:
                 hashmap[key] = [string]
         return list(hashmap.values())
+
+
+# array is not hashable but tuple is
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            freq = [0] * 26   
+            for c in word:
+                freq[ord(c) - ord('a')] += 1
+            key = tuple(freq)   # tuples are hashable
+            groups[key].append(word)
+
+        return list(groups.values())
