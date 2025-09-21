7d4ccfd87a6b467e1f2eeb8ee94c053d6ecafcaa
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-09-21 15:20:34 -0400

    876

--- 876. Middle of the Linked List.py
+++ 876. Middle of the Linked List.py
@@ -3,11 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        fast = slow = head
+        
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+        # For odd-length list → slow ends on the true middle.
+        # For even-length list → slow ends on the second middle.
         return slow
+
+        """
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        For odd-length list → slow ends on the true middle.
+        For even-length list → slow ends on the first middle.
+        """
