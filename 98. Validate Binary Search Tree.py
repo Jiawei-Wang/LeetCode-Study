ef777710a54cfe6d8e49582f2f77c0557a4531ac
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-09-22 13:25:22 -0400

    98

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -61,3 +61,26 @@ class Solution:
     比如root的左子node的限制为：-inf < value < root.val
     假设root = 10, 此node = 5，则它的右子node的限制为 5 < value < 10
     '''
+
+
+# 2025
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def isValid(node, left, right):
+            if not node:
+                return True
+            
+            if left < node.val < right:
+                return isValid(node.left, left, node.val) and isValid(node.right, node.val, right)
+            else:
+                return False
+            
+        return isValid(root, -float("inf"), float("inf"))
+        
