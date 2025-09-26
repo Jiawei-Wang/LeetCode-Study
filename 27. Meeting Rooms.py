0bdaba001451379b7b857d63f95e2882e8e7bfa4
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-09-25 22:20:41 -0400

    27

--- 27. Meeting Rooms.py
+++ 27. Meeting Rooms.py
@@ -30,3 +30,25 @@ class Solution:
                 return False
         
         return True
+
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not len(intervals):
+            return True
+
+        intervals.sort(key = lambda x: x.start)
+        current = intervals[0].end
+        for i in range(1, len(intervals)):
+            if intervals[i].start < current:
+                return False
+            current = intervals[i].end
+        return True
