1fad89c3407d88deb027a8a8888a5d676f4eb4fe
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-10-13 23:58:20 -0400

    2706

--- 2706. Buy Two Chocolates.py
+++ 2706. Buy Two Chocolates.py
@@ -13,4 +13,18 @@ class Solution:
         return left if left >= 0 else money
 
 
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        first = second = float("inf")
+        for price in prices:
+            if price < first:
+                first, second = price, first
+            elif price < second:
+                second = price
         
+        leftover = money - first - second
+        
+        if leftover >= 0:
+            return leftover
+        else:
+            return money
