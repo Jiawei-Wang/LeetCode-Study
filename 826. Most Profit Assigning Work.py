9320424a4cd36c3085ddc1788042c3f056e7a123
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-10-23 10:35:23 -0400

    826

---
+++ 826. Most Profit Assigning Work.py
@@ -0,0 +1,12 @@
+# sort difficulty and worker: nlogn + mlogm
+# then go through difficulty and worker: n + m
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(difficulty, profit))
+        res = i = best = 0
+        for ability in sorted(worker):
+            while i < len(jobs) and ability >= jobs[i][0]:
+                best = max(jobs[i][1], best)
+                i += 1
+            res += best
+        return res
