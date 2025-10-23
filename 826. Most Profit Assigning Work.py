7fe64c096a810b5d73dadc1156c4c275d639715b
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-10-23 10:53:15 -0400

    826

--- 826. Most Profit Assigning Work.py
+++ 826. Most Profit Assigning Work.py
@@ -10,3 +10,27 @@ class Solution:
                 i += 1
             res += best
         return res
+
+
+# create a map then binary search on it
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        # Create a map (difficulty -> best profit for that difficulty)
+        jobs = dict()
+        for d, p in zip(difficulty, profit):
+            jobs[d] = max(jobs.get(d, 0), p)
+        diffs = sorted(jobs.keys())
+        best = 0
+        for d in diffs:
+            best = max(best, jobs[d])
+            jobs[d] = best  
+
+        # Binary search
+        diff_list = diffs
+        profit_list = [jobs[d] for d in diff_list]
+        res = 0
+        for w in worker:
+            i = bisect_right(diff_list, w) - 1
+            if i >= 0:
+                res += profit_list[i]
+        return res
