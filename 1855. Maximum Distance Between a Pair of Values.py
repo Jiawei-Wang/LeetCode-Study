f00d7ccadc3a99f430ec8fc3c443fdc4844ee1d1
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-11-26 14:23:00 -0500

    1855

--- 1855. Maximum Distance Between a Pair of Values.py
+++ 1855. Maximum Distance Between a Pair of Values.py
@@ -21,3 +21,22 @@ class Solution:
                 ans = max(ans, best - i)
 
         return ans
+
+
+# two pointer
+class Solution:
+    def maxDistance(self, nums1, nums2):
+        i = j = 0
+        n1, n2 = len(nums1), len(nums2)
+        ans = 0
+
+        while i < n1 and j < n2:
+            if j < i:
+                j = i
+            elif nums1[i] <= nums2[j]:
+                ans = max(ans, j - i)
+                j += 1
+            else:
+                i += 1
+
+        return ans
