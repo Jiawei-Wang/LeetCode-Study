6bcf6c9c28f4bbd919c36b296c234a7b015722cf
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-11-30 18:25:57 -0500

    1374

---
+++ 1374. Generate a String With Characters That Have Odd Counts.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n == 1:
+            return "a"
+            
+        if n % 2 == 0:
+            return "a" * (n-1) + "b"
+        else:
+            return "a" * (n-2) + "b" + "c"
