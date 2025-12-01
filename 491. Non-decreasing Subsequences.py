cb286977a505114ef8187205ede816d51cd29553
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-12-01 14:20:12 -0500

    491

--- 491. Non-decreasing Subsequences.py
+++ 491. Non-decreasing Subsequences.py
@@ -26,3 +26,29 @@ class Solution:
 
         dfs(0)
         return res
+
+
+# use local path: 
+# space: O(n^2)
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+
+        def dfs(start, path): # start: the index of the number we are standing on
+            # first check if requirement is satisfied
+            if len(path) >= 2:
+                answer.append(path)     # no need to copy, we never mutate path
+
+            used = set()
+
+            # then pick next number
+            for i in range(start, len(nums)):
+                if (path and nums[i] < path[-1]) or nums[i] in used:
+                    continue
+
+                used.add(nums[i])
+                # pass a NEW path (no mutation)
+                dfs(i + 1, path + [nums[i]])
+
+        dfs(0, [])
+        return answer
