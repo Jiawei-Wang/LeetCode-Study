a52b8a86cd45d8f43bc4a0b84fa3c1e429329e1b
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-12-01 09:24:45 -0500

    453

--- 453. Minimum Moves to Equal Array Elements.py
+++ 453. Minimum Moves to Equal Array Elements.py
@@ -1,3 +1,13 @@
+# if we take m moves to make every number the same:
+# target * len(nums) = sum(nums) + m * (len(nums) - 1)
+# and 
+# target = min(nums) + m
+# (regardless which len(nums) - 1 numbers we pick for each round of increment
+#  the minimum number will always be part of the pick, so it will always stay
+#  as the minimum number, therefore target is m steps away from min(nums))
+# so
+# sum(nums) - min(nums) * len(nums) = m 
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        return sum(nums) - min(nums) * len(nums) 
+        return sum(nums) - min(nums) * len(nums)
+        
