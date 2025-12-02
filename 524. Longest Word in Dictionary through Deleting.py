b424389c12a0779c8dd26419970cd95d79241851
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-12-01 19:23:44 -0500

    524

--- 524. Longest Word in Dictionary through Deleting.py
+++ 524. Longest Word in Dictionary through Deleting.py
@@ -21,3 +21,36 @@ class Solution:
             if can_transform(s, word):
                 return word        
         return ""
+
+
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        length = len(s)
+        
+        def is_in(word):
+            i = 0
+            j = 0
+
+            while i <= len(word) and j <= length:
+                if i == len(word) and j == length:
+                    return True
+                elif j == length:
+                    return False
+                elif i == len(word):
+                    return True
+                
+                if word[i] == s[j]:
+                    i += 1
+                
+                j += 1
+
+            return False
+        
+        # first sort by length, longer ones first
+        # then for same length, sort by lexicographical order
+        dictionary.sort(key = lambda x : (-len(x), x))
+        for word in dictionary:
+            if is_in(word):
+                return word
+        
+        return ""
