340fd1e903f191f9ae521aba89a0f98fcd1df62d
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-12-04 12:35:29 -0500

    1577

--- 1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers.py
+++ 1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers.py
@@ -48,3 +48,54 @@ class Solution:
                     res += d1[p]
 
         return res
+
+
+# 2025
+class Solution:
+    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        # Build a frequency dictionary
+        def freq(arr):
+            mp = {}
+            for x in arr:
+                mp[x] = mp.get(x, 0) + 1
+            return mp
+
+        freq1 = freq(nums1)
+        freq2 = freq(nums2)
+        
+        ans = 0
+
+        # Given target = x^2, count number of (a, b) from freq map where a * b == target
+        def count_pairs(freq_map, target):
+            total = 0
+            for a in freq_map:
+                if target % a != 0:
+                    continue
+
+                b = target // a
+                if b not in freq_map:
+                    continue
+
+                # Case 1: distinct a and b → freq[a] * freq[b]
+                if a < b:
+                    total += freq_map[a] * freq_map[b]
+
+                # Case 2: a == b → combinations C(n, 2) = n*(n-1)//2
+                elif a == b:
+                    n = freq_map[a]
+                    total += n * (n - 1) // 2
+
+            return total
+        
+        
+        # Count nums1[i]^2 == nums2[j] * nums2[k]
+        for x in freq1:
+            target = x * x
+            ans += freq1[x] * count_pairs(freq2, target)
+        
+        # Count nums2[i]^2 == nums1[j] * nums1[k]
+        for x in freq2:
+            target = x * x
+            ans += freq2[x] * count_pairs(freq1, target)
+        
+        return ans
