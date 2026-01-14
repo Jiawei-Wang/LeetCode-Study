810daa14b7f2868e79aa299185b1590013d22611
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-13 20:27:54 -0500

    334

--- 334. Increasing Triplet Subsequence.py
+++ 334. Increasing Triplet Subsequence.py
@@ -18,3 +18,17 @@ understanding:
 3. first may point to something else (for example: [1,0,2,0,-1,3] where first will eventually point to -1)
 4. but as long as we have a valid second and have another number larger than second, we can return true
 """
+
+
+# walk through the array with 3 flags
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float("inf")
+        for n in nums:
+            if n <= first: # 
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
