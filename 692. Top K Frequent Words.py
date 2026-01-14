f4470b6c3f86f4e9ac45bdca5d10edd164a781c0
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-13 20:27:57 -0500

    692

--- 692. Top K Frequent Words.py
+++ 692. Top K Frequent Words.py
@@ -28,3 +28,35 @@ class Word:
     
     def __eq__(self, other):
         return self.freq == other.freq and self.word == other.word
+
+
+# 2026
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = [set() for _ in range(len(words)+1)]
+        # freq[3] = {"I", "love"}
+        # I and love both appeared 3 times
+        lookup = defaultdict(int) # key: word, value: number of occurrences
+
+        for word in words:
+            if lookup[word] != 0:
+                freq[lookup[word]].remove(word)
+            lookup[word] += 1
+            freq[lookup[word]].add(word)
+        # for example words = ["i","love","leetcode","i","love","coding"]
+        # freq = [set(), {'leetcode', 'coding'}, {'love', 'i'}, set(), set(), set(), set()]
+    
+        answer = []
+        index = len(freq) - 1
+        while k != 0 and index >= 0:
+            current = freq[index]
+            candidates = sorted([word for word in current])
+            picked = candidates[:min(k, len(candidates))] # if we have 3 more to pick but have 4 candidates, we can only pick 3
+            answer.extend(picked)
+            k -= len(picked)
+            index -= 1
+        return answer
+
+
+
+        
