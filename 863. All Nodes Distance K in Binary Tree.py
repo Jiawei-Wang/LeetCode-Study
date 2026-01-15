4c1f14d0f7439e1371ead19bdcf77f723909d0dd
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-15 15:04:53 -0500

    863

--- 863. All Nodes Distance K in Binary Tree.py
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -41,3 +41,46 @@ class Solution:
             # add all the values in bfs into seen
             seen |= set(bfs)
         return bfs
+
+
+# 2026
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        adj = defaultdict(set) # key: value of a node, value: list of values of all neighbor nodes
+        lookup = dict() # map node's value to the node
+        
+        # go through all nodes in the tree
+        current_level = [root]
+        lookup[root.val] = root
+
+        while current_level:
+            next_level = []
+            for node in current_level:
+                if node.left:
+                    next_level.append(node.left)
+                    lookup[node.left.val] = node.left
+                    adj[node.val].add(node.left.val)
+                    adj[node.left.val].add(node.val)
+                if node.right:
+                    next_level.append(node.right)
+                    lookup[node.right.val] = node.right
+                    adj[node.val].add(node.right.val)
+                    adj[node.right.val].add(node.val)
+            current_level = next_level
+        
+        # bfs from target node
+        visited = set()
+        visited.add(target.val)
+        current = set()
+        current.add(target.val)
+        while k != 0:
+            nxt = set()
+            for value in current:
+                for neighbor in adj[value]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        nxt.add(neighbor)
+            current = nxt
+            k -= 1
+
+        return list(current)
