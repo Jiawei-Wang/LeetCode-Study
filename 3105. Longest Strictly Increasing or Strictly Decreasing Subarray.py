e52eaca38cda63d294349da70b86cd47e0264bad
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-23 15:30:54 -0500

    3105

---
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        inc = dec = 1
+        answer = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                inc += 1
+                dec = 1
+            elif nums[i] < nums[i-1]:
+                inc = 1
+                dec += 1
+            else:
+                inc = dec = 1
+            answer = max(answer, dec, inc)
+        return answer
