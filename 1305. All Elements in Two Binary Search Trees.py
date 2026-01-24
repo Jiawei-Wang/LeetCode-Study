348b052e8e1c9a00b3029a5545e5590920098c8b
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-24 10:04:39 -0500

    1305

--- 1305. All Elements in Two Binary Search Trees.py
+++ 1305. All Elements in Two Binary Search Trees.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# traversal first + merge later
 class Solution:
     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
         # in order traversal 
@@ -31,4 +34,31 @@ class Solution:
 
         res.extend(a[i:])
         res.extend(b[j:])
+        return res
+
+
+# traversal while merging
+class Solution:
+    def getAllElements(self, root1, root2):
+        def push_left(node, stack):
+            while node:
+                stack.append(node)
+                node = node.left
+
+        s1, s2 = [], []
+        push_left(root1, s1)
+        push_left(root2, s2)
+
+        res = []
+        while s1 or s2:
+            # choose which stack has smaller next value
+            if not s2 or (s1 and s1[-1].val <= s2[-1].val):
+                node = s1.pop()
+                res.append(node.val)
+                push_left(node.right, s1)
+            else:
+                node = s2.pop()
+                res.append(node.val)
+                push_left(node.right, s2)
+
         return res
