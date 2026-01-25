cf88ea0e7c2dcfc49eb526955ea875a6c81accf2
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-01-25 17:31:20 -0500

    2733

--- 2733. Neither Minimum nor Maximum.py
+++ 2733. Neither Minimum nor Maximum.py
@@ -19,3 +19,13 @@ class Solution:
         
         return -1
             
+
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return -1
+            
+        a = nums[0]
+        b = nums[1]
+        c = nums[2]
+        return a + b + c - min(a, b, c) - max(a, b, c)
