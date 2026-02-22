ed05ea8081bd8412618edda6ce34993c96cf369c
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-02-22 09:06:18 -0500

    268

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,16 +1,22 @@
+# we have n unique numbers in the range [0, n]
+# which means there is one missing number 
+# find that missing number
+# for example: [9,6,4,2,3,5,7,0,1]
+# in total 9 numbers between [0, 9]
+# and the missing one is 8
+
+
 # brute force: nlogn
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        if nums[0]:
-            return 0
-        for i in range(1,len(nums)):
-            if nums[i] - nums[i-1] == 2:
-                return nums[i]-1
-        return nums[-1]+1
+        for index, number in enumerate(nums):
+            if index != number:
+                return index
+        return len(nums) # in case the missing number is n
 
 
-# n
+# using a set: n
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         ans = set(i for i in range(len(nums)+1))
@@ -31,30 +37,15 @@ class Solution:
 # any number ^ 0 = itself
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        start = 0
-        for i in range(len(nums)):
-            start = start ^ i ^ nums[i]
-        i += 1
-        return start ^ i
-    
-"""
-从n个元素中找到缺失的那一个，例：
-[3,0,2]: 缺少1
-len(nums) = 3, index = [0,1,2]
-所以for循环中是：start = 0 ^ (0^1^2) ^ (0^2^3)
-如果把它看成是4次循环而不是3次，则可以理解最后结尾时有一个index没有对应值，它就是答案
-所以 ^ len(nums)+1 可以得到该值
-"""
-
-
-# 2024
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
+        n = len(nums)
         xor = 0
-        for i in range(length):
+        for i in range(n):
             xor ^= i
             xor ^= nums[i]
-        xor ^= length
+        # for example [3, 0, 1]
+        # index = [0, 1, 2]
+        # so 0 matches 0, 1 matches 1, index 2 and value 3 don't match
+        # so we just need to ^= 3 to get 2 
+        xor ^= n
         return xor
 
