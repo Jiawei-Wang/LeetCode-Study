84c120bd80924463f08407191058fae1cdad328a
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-03-06 09:14:13 -0500

    200

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -214,3 +214,26 @@ class Solution:
                     count += 1
                     dfs(i, j)
         return count
+
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+
+        def dfs(i, j):
+            if 0 <= i < row and 0 <= j < col and grid[i][j] == "1":
+                grid[i][j] = "0"
+                dfs(i, j+1)
+                dfs(i, j-1)
+                dfs(i-1, j)
+                dfs(i+1, j)
+        
+        counter = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1":
+                    counter += 1
+                    dfs(i, j)
+        return counter
