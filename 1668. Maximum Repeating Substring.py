1932fa2c4cd3d099783ca3bf088cbe3c62b5c28f
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-04-28 15:01:06 -0400

    1668

---
+++ 1668. Maximum Repeating Substring.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def maxRepeating(self, sequence: str, word: str) -> int:
+#         s = len(sequence)
+#         w = len(word)
+
+#         counter = 0
+#         global_longest = 0
+        
+#         index = 0
+#         while index <= s - w:
+#             if sequence[index:index+w] == word:
+#                 counter += 1
+#                 global_longest = max(counter, global_longest)
+#                 index += w
+#             else:
+#                 counter = 0
+#                 index += 1
+#         return global_longest
+
+
+        """
+        above answer fails 1 test case out of 212:
+        sequence = "aaabaaaabaaabaaaabaaaabaaaabaaaaba"
+        word = "aaaba"
+        Output 4
+        Expected 5
+        """
