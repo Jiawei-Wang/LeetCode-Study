5707b39bf47d0c8f69c6f04556b1fd9ae67b7337
Jiawei Wang <37646262+Jiawei-Wang@users.noreply.github.com>
2026-05-27 14:38:35 -0400

    2180

--- 2180. Count Integers With Even Digit Sum.py
+++ 2180. Count Integers With Even Digit Sum.py
@@ -8,3 +8,23 @@ class Solution:
             if digit_sum % 2 == 0:
                 count += 1
         return count
+
+
+"""
+in any consecutive block of 10 numbers (for example 0-9 or 10-19)
+there are 5 even digit sum numbers, 5 odd digit sum numbers
+so for an number num:
+we have num//10 blocks, each has 5 
+
+if num itself has even digit sum, then from 1 to num we have num//2 in total
+if num itself has odd digit sum, then from 1 to num we have (num-1)//2 in total
+
+overall: (num- (digit_)
+"""
+class Solution:
+    def countEven(self, num: int) -> int:
+        # Calculate the digit sum of num
+        digit_sum = sum(int(d) for d in str(num))
+
+        # Apply the math trick
+        return (num - (digit_sum % 2)) // 2
