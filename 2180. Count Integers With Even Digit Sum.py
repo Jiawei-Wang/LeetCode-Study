0032a148624f2299e5c9fe62de4548e9a41941b5
Jiawei Wang <wangjiawei_med@hotmail.com>
2026-05-27 10:53:02 -0400

    2180

---
+++ 2180. Count Integers With Even Digit Sum.py
@@ -0,0 +1,10 @@
+# simulation
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+        for i in range(1, num + 1):
+            # Calculate digit sum
+            digit_sum = sum(int(digit) for digit in str(i))
+            if digit_sum % 2 == 0:
+                count += 1
+        return count
