9e08b9c5ce8c2f10c0be7399dd82550767823cc5
Jiawei Wang <37646262+Jiawei-Wang@users.noreply.github.com>
2026-07-02 11:37:04 -0400

    209

--- 209. Minimum Size Subarray Sum.py
+++ 209. Minimum Size Subarray Sum.py
@@ -89,3 +89,98 @@ class Solution:
                 left += 1
 
         return result if result <= len(nums) else 0
+
+
+
+
+# 2026
+# brute force: pick all possible pairs, calculate sum for each pair n^3 
+# prefix sum: pick all possible pairs, check sum_j - sum_i n^2
+# binary search: for each i, find the cloest j behind it using prefix sum nlogn
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        for example: nums = [a, b, c, d]
+        all possible subarrays are:
+        starting at a: a, ab, abc, abcd
+        starting at b: b, bc, bcd
+        starting at c: c, cd
+        starting at d: d
+        to get sum(abc), we can't use prefix[c] - prefix[a]
+        that will give a + b + c - a = b + c
+        instead we need prefix[c] - prefix[0]: a + b + c - 0
+        so if we need the sum of subarray nums[i:j+1]
+        we need prefix[j] - prefix[i-1]
+        """
+        prefix = [0] # empty subarray has total = 0
+
+        total = 0
+        for num in nums:
+            total += num
+            prefix.append(total)
+        # for example prefix[3] = 10 means
+        # first 3 elements at index = 0, 1, 2 have total = 10
+        # nums[0:3] = 10
+
+        # if sum of whole nums is still to small
+        if prefix[-1] < target:
+            return 0
+        
+        # else: we know there exists such subarray
+        length = len(nums) 
+        minimum_length = length # this subarray can be at most the whole nums
+
+        for i in range(length):
+        # prefix[i] is the part that will be deducted so i can go from
+        # 0: empty subarray, to
+        # length - 1: subarray with everything but the last element in nums
+        # it makes no sense to add last element because that gives the full nums
+        # then the only j is just prefix[-1] 
+        # and prefix[j] - prefix[i] = 0 (whole nums - whole nums = nothing inside)
+
+            # now we use binary search to find the smallest j such that 
+            # prefix[j] - prefix[i] >= target
+            left = i + 1 #  j starts at at least i+1 (1 element in the final subarray)
+            right = length # ends at length (all elements in nums are in prefix[j])
+            # len(prefix) = len(nums) + 1
+            # so last element in prefix is prefix[length]
+
+            while left != right:
+                mid = (right - left) // 2 + left 
+                subarray = prefix[mid] - prefix[i] 
+                if subarray == target: # everything element in nums is positive so the subarray can't get shorter
+                    minimum_length = min(minimum_length, mid - i)
+                    break
+                elif subarray > target: # try to shrink 
+                    right = mid
+                else: # try to expand
+                    left = mid + 1
+            
+            # when left and right become the same, the subarray[i+1: right+1] may still be < target
+            if prefix[right] - prefix[i] >= target:
+                minimum_length = min(minimum_length, right - i)
+
+        
+        return minimum_length 
+
+# slinding window: n
+# expanding right then shrinking left is too complicated
+# we should only care about right
+# for every step right takes, find the biggest left
+class Solution:
+    def minSubArrayLen(self, target, nums):
+        result = len(nums) + 1
+        
+        left = 0
+        total = 0
+        
+        for right, n in enumerate(nums):
+            total += n
+            while total >= target:
+                # instead of updating result at the very last
+                # update it for every step left takes
+                result = min(result, right - left + 1)
+                total -= nums[left]
+                left += 1
+
+        return result if result <= len(nums) else 0
